9703f6897e7205a93a575754f21bfaa478aca430
gabo0802 <gabriel.castejon@ufl.edu>
2023-01-22 15:27:49 -0500

    golang import

--- hello.go
+++ hello.go
@@ -4,8 +4,12 @@ import (
 	"fmt"
 
 	"github.com/gabo0802/Go-AngularTest/StringUtilLibrary"
+	"golang.org/x/example/stringutil"
 )
 
 func main() {
 	fmt.Println(StringUtilLibrary.Reverse("!oG ,olleH"))
+
+	fmt.Println(stringutil.Reverse("sllab ,olleH"))
+
 }
